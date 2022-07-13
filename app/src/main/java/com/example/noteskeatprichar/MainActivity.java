b9ed3833d1053de439fcece93ada5db37cd7797f
jkeatpri <108395205+jkeatpri@users.noreply.github.com>
2022-07-13 23:16:10 +0800

    Milestone 1: Add adapter and functions

--- app/src/main/java/com/example/noteskeatprichar/MainActivity.java
+++ app/src/main/java/com/example/noteskeatprichar/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.noteskeatprichar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements EditNoteDialogFragment.EditNoteDialogListener{
+    ArrayList<Note> notes;
+    NotesAdapter notes_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListAdapterMethos();
+        btnAddListenerMethod();
+        etNoteEnterListenerMethod();
     }
+
+    private void etNoteEnterListenerMethod(){
+        EditText etNote = findViewById(R.id.etNote);
+        etNote.setOnKeyListener(new View.OnKeyListener()
+        @Override
+        public boolean onKey(View view, int i, KeyEvent keyEvent){
+            if(keyEvent.getAction() == KeyEvent.ACTION_DOWN || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER || keyEvent.getAction() == KeyEvent.KEYCODE_NUMPAD_ENTER){
+                String note = etNote.getText().toString();
+                notes.add(new Note(note));
+                notes_adapter.notifyDataSetChanged();
+                etNote.setText("");
+                return true;
+            }
+            return false;
+        }
+    });
+}
+
+    private void btnAddListenerMethod(){
+    Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            EditText etNote = findViewById(R.id.etNote);
+            String note = etNote.getText().toString();
+            notes.add(new Note(note));
+            notes_adapter.notifyDataSetChanged();
+            etNote.setText("");
+        }
+    });
+}
+
+    private void setListAdapterMethod(){
+        ListView lvList = findViewById(R.id.lvList);
+        notes = new ArrayList<>();
+        notes.add(new Note("First Note"));
+        notes.add(new Note("Second Note"));
+
+        notes_adapter new NotesAdapter(getBaseContext(), R.layout.note_layout, notes, getSupportFragmentManager());
+        lvList.setAdapter(notes_adapter);
+
+        notes.add(new Note("Joshua Caballero Keatprichar"));
+
+}
+
+    @Override
+    public void onEditListenerMethod(DialogFragment dialog){
+        notes_adapter.onEditListenerMethod(dialog);
+}
+
+    @Override
+    public void onCancelListenerMethod(DialogFragment dialog){
+    notes_adapter.onCancelListenerMethod(dialog);
+    }
+
 }
